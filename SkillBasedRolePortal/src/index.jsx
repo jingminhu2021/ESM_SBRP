dba9bf0777cd1a552f80b0192995a25e9096c577
Disquietudee <50265869+Disquietudee@users.noreply.github.com>
2023-09-17 21:21:19 +0800

    feat: [IGT-3] Changed to vite react js+swc

---
+++ SkillBasedRolePortal/src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/css/flaticon.css'
+import './assets/css/price_rangs.css'
+import './assets/css/slicknav.css'
+import './assets/css/animate.min.css'
+import './assets/css/magnific-popup.css'
+import './assets/css/fontawesome-all.min.css'
+import './assets/css/themify-icons.css'
+import './assets/css/slick.css'
+import './assets/css/nice-select.css'
+import './assets/css/style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
