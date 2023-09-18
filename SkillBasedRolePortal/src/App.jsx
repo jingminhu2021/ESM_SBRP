aad83a7d2d26853454fe381b121ac182221d8664
Disquietudee <50265869+Disquietudee@users.noreply.github.com>
2023-09-18 22:21:36 +0800

    fix: [IGT-3] fixed css issue and navbar for responsiveness

--- SkillBasedRolePortal/src/App.jsx
+++ SkillBasedRolePortal/src/App.jsx
@@ -1,8 +1,13 @@
 // import './App.css'
 import navbar from './components/navbar.jsx'
 import home from './components/home.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery-nice-select/css/nice-select.css'
+import './assets/css/custom-bs.css'
+import './assets/css/jquery.fancybox.min.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/fonts/icomoon/style.css'
+import './assets/fonts/line-icons/style.css'
+import './assets/css/animate.min.css'
+import './assets/css/style.css'
 
 function App() {
   return (
--- SkillBasedRolePortal/src/index.jsx
+++ SkillBasedRolePortal/src/index.jsx
@@ -9,16 +9,10 @@ import App from './App.jsx'
 // import './assets/css/magnific-popup.css'
 // import './assets/css/fontawesome-all.min.css'
 // import './assets/css/themify-icons.css'
-import './assets/css/animate.min.css'
-import './assets/css/bootstrap-select.min.css'
-import './assets/css/custom-bs.css'
-import './assets/css/jquery.fancybox.min.css'
-import './assets/css/owl.carousel.min.css'
-import './assets/css/quill.snow.css'
-import './assets/css/style.css'
-import './assets/css/style2.css'
-import './assets/fonts/icomoon/style.css'
-import './assets/fonts/line-icons/style.css'
+
+// import './assets/css/bootstrap-select.min.css'
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
