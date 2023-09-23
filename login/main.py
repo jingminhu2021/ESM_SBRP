988f172c09f03a355d681054f8d806664118edfd
Disquietudee <50265869+Disquietudee@users.noreply.github.com>
2023-09-23 17:07:45 +0800

    feat: [IGT-6] change port to 5000

--- login/main.py
+++ login/main.py
@@ -90,4 +90,4 @@ def logout():
     return redirect("/")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5003, debug=True) #testing purpose
+    app.run(host="0.0.0.0", port=5000, debug=True) #testing purpose
