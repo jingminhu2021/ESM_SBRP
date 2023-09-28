5146b185f600ec07ed74d58d20b1a4ea7fb18712
Jingmin <112000981+jingminhu2021@users.noreply.github.com>
2023-09-29 02:49:16 +0800

    feat: [IGT-11] check if hr and cancel button

--- SkillBasedRolePortal/src/pages/createSkill.jsx
+++ SkillBasedRolePortal/src/pages/createSkill.jsx
@@ -2,11 +2,39 @@ import { useState, useEffect} from "react";
 import navbar from '../components/navbar.jsx';
 import axios from 'axios';
 import Select from 'react-select'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link} from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function createSkill() {
+  // Check if HR is logged in
+  if (sessionStorage.getItem('sys_role') != 'hr') {
+    return (
+        <div>
+            {navbar()}
+            <section className="section-hero overlay inner-page bg-image" style={{ backgroundImage: 'url(/images/hero_1.jpg)' }} id="home-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-7">
+                            <h1 className="text-white font-weight-bold">-</h1>
+                            <div className="custom-breadcrumbs">
+                                <a href="/">Home</a><span className="mx-2 slash">/</span>
+                                <Link to="/ViewSkills">Skills</Link><span className="mx-2 slash">/</span>
+                                <span className="text-white"><strong>-</strong></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="site-section" id="next-section">
+                <div className="container text-center">
+                    <p className="font-weight-bold" style={{ fontSize: '24px' }}>You are not authorized to view this page!</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+else {
   const [skillName, setSkillName] = useState('');
   const [skillDescription, setSkillDescription] = useState('');
   const [allSkills, setAllSkills] = useState([]);
@@ -179,6 +207,9 @@ function createSkill() {
                   <div className="col-md-auto">
                     <input type="submit" value="Create Skill" className="btn btn-primary btn-md text-white col-md-12" />
                   </div>
+                  <div className="col-md-auto">
+                    <Link to={'/ViewSkills'} className="btn btn-secondary btn-md col-md-12">Cancel</Link>
+                  </div>
                 </div>
               </form>
 
@@ -188,5 +219,6 @@ function createSkill() {
         </section>
         </div>
     );
+  }
 }
 export default createSkill
