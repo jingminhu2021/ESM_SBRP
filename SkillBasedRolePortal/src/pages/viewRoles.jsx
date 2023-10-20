a2a380405b86c6629dc3ca15e1ab7dd4843d8bab
Ong Jun Yang <50265869+Disquietudee@users.noreply.github.com>
2023-10-20 22:43:13 +0800

    feat: [IGT-7] changed role_listing (#53)

--- SkillBasedRolePortal/src/pages/viewRoles.jsx
+++ SkillBasedRolePortal/src/pages/viewRoles.jsx
@@ -9,9 +9,12 @@ function RoleListings() {
     
     const [rolelistings, setRoles] = useState([]);
     const location = useLocation();
-
+    const api_link = 'http://localhost:8000/api/role/view_role_listings_hr'
+    if (sessionStorage.getItem('sys_role') === 'manager') {
+        api_link = 'http://localhost:8000/api/role/view_role_listings_manager/' + sessionStorage.getItem('staff_id')
+    }
     useEffect(() => {
-        axios.get('http://localhost:8000/api/role/view_role_listings')
+        axios.get(api_link)
         .then(response => {
             setRoles(response.data.data);
             console.log(response.data.data);
